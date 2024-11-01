f6f147fef3e81444ed78ee600858b6384a41ca68
Yuta <jp.yuta0131@gmail.com>
2024-11-02 06:28:42 +0900

    update

--- buildiso.sh
+++ buildiso.sh
@@ -1,1 +1,10 @@
-sudo mkarchiso -v -r profile
+#!/bin/bash
+
+MAKEFLAGS="-j$(nproc)"
+
+sudo mount -t tmpfs -o size=8G tmpfs /mnt/ramdisk
+
+sudo mkarchiso -v -w /mnt/ramdisk profile
+
+sudo umount /mnt/ramdisk
+sudo rm -rf work
